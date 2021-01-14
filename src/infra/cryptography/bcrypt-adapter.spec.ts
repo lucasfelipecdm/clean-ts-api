d71abc36738f3efc056f126b6f0dbb277e6e1fcf
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2021-01-13 21:56:11 -0300

    test: ensure BcryptAdapter throws if Bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -24,4 +24,15 @@ describe('Bcrypt Adapter', () => {
     const hashedValue = await sut.encrypt('any_value');
     expect(hashedValue).toBe('hashed_value');
   });
+
+  test('Should throw if Bcrypt throws', async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, 'hash')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const hashedValue = sut.encrypt('any_value');
+    await expect(hashedValue).rejects.toThrow();
+  });
 });
